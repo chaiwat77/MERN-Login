8dde5c1fda6c1402caf98d86ede315a302095fba
chaiwat77 <123268098+chaiwat77@users.noreply.github.com>
2023-05-29 23:41:37 +0700

    user page update

--- client/src/component/layouts/MenubarUser.js
+++ client/src/component/layouts/MenubarUser.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { HeartOutlined, HistoryOutlined } from "@ant-design/icons";
 
 const MenubarUser = () => {
   return (
-    <nav>
-      <ul class="nav flex-column">
-        <li class="nav-item">
-          <Link to="/user/wishlist">สินค้าที่สนใจ</Link>
-        </li>
-        <li class="nav-item">
-          <Link to="/user/history">ประวัติการสั่งซื้อ</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <div className="d-flex-fluid">
+        <div className="list-group mt-3">
+          <nav className="nav flex-column">
+            <Link
+              to="/user/wishlist"
+              className="nav-link list-group-item list-group-item-action"
+            >
+              <HeartOutlined className="me-2" />
+              สินค้าที่สนใจ
+            </Link>
+            <Link
+              to="/user/history"
+              className="nav-link list-group-item list-group-item list-group-item-action"
+            >
+              <HistoryOutlined className="me-2" />
+              จัดการผู้ใช้งาน
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </>
   );
 };
 
--- client/src/component/pages/user/WishList.js
+++ client/src/component/pages/user/WishList.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import MenubarUser from "../../layouts/MenubarUser";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
+import { Button, ListGroup } from "react-bootstrap";
+import { Heart } from "react-bootstrap-icons";
 
 import { getWishList, removeToWishList } from "../../functions/users";
 import { Link } from "react-router-dom";
@@ -15,12 +17,10 @@ const WishList = () => {
   useEffect(() => {
     loadData();
   }, []);
-  console.log(wishlist);
 
   const loadData = () => {
     getWishList(user.token).then((res) => {
       setWishList(res.data.wishlist);
-      // console.log(res.data.wishlist);
     });
   };
 
@@ -30,31 +30,66 @@ const WishList = () => {
       loadData();
     });
   };
-  return (
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-2">
-          <MenubarUser />
-        </div>
 
-        <div class="col">
-          <div className="row">
-            <h2>Wishlist Page</h2>
-            {wishlist.map((item, index) => (
-              <div key={index} className="alert alert-secondary">
-                <Link to={"/product/" + item._id}>{item.title}</Link>
-                <span
-                  style={{ float: "right" }}
-                  onClick={() => handleRemove(item._id)}
-                >
-                  Remove List
-                </span>
-              </div>
-            ))}
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-2">
+            <MenubarUser />
+          </div>
+
+          <div className="col">
+            <div className="row">
+              <h2>Wishlist Page</h2>
+              <ListGroup>
+                {wishlist.map((item, index) => (
+                  <ListGroup.Item
+                    key={index}
+                    className="d-flex justify-content-between align-items-center"
+                  >
+                    <Link to={"/product/" + item._id}>{item.title}</Link>
+                    <Button
+                      variant="danger"
+                      onClick={() => handleRemove(item._id)}
+                    >
+                      <Heart />
+                      Remove List
+                    </Button>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {/* <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-2">
+            <MenubarUser />
+          </div>
+
+          <div class="col">
+            <div className="row">
+              <h2>Wishlist Page</h2>
+              {wishlist.map((item, index) => (
+                <div key={index} className="alert alert-secondary">
+                  <Link to={"/product/" + item._id}>{item.title}</Link>
+                  <Button
+                    variant="danger"
+                    onClick={() => handleRemove(item._id)}
+                    style={{ float: "right" }}
+                  >
+                    <Heart />
+                    Remove List
+                  </Button>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </>
   );
 };
 
